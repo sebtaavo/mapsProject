1e52ac4f4736d55c398aec4f3fa5750f75bad0d0
SimonLieb <simonlf@ug.kth.se>
2024-11-25 23:51:06 +0100

    Extreme cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite';
 import vueJsxPlugin from '@vitejs/plugin-vue-jsx';
 import vuePlugin from '@vitejs/plugin-vue';
+import path from 'path'; // Import path module
 
 export default defineConfig({
     plugins: [
         vueJsxPlugin(),
-       vuePlugin(),
+        vuePlugin(),
     ],
     server: {
         port: 8080,
     },
     build: {
         sourcemap: true,
-        minify: false
-    }
+        minify: false,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // Define '@' as an alias for 'src'
+        },
+    },
 });
