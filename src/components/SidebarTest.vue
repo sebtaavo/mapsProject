e655aeaf92eb1f941e0a7421b981d920d857cc8f
SimonLieb <simonlf@ug.kth.se>
2024-12-13 20:54:08 +0100

    Copy button + dropdown css

--- src/components/SidebarTest.vue
+++ src/components/SidebarTest.vue
@@ -1,103 +1,115 @@
 <template>
-    <div class="sidebar" v-if="!place">
-      <div class="group-members" v-if="groupMembers">
-        <h2 class="heading">Group Members</h2>
-        <ul class="member-list" >
-          <li v-if="!user" class="member-item">
-            Please log in to use the app.
-          </li>
-          <li v-else-if="groupMembers.length === 0" class="member-item">
-            Join or create a group to get started!
-          </li>
-          <li
-            v-if="user"
-            v-for="(member, index) in groupMembers"
-            :key="member.uid"
-            class="member-item"
-            @click="handleMemberWasClicked(member)"
+  <div class="sidebar" v-if="!place">
+    <div class="group-members" v-if="groupMembers">
+      <h2 class="heading">Group Members</h2>
+      <ul class="member-list">
+        <li v-if="!user" class="member-item">
+          Please log in to use the app.
+        </li>
+        <li v-else-if="groupMembers.length === 0" class="member-item">
+          Join or create a group to get started!
+        </li>
+        <li
+          v-if="user"
+          v-for="(member, index) in groupMembers"
+          :key="member.uid"
+          class="member-item"
+          @click="handleMemberWasClicked(member)"
+        >
+          <img :src="member.icon || 'path_to_default_icon_or_gravatar'" class="member-icon" alt="User Icon" />
+          {{ member.name }}
+          <span v-if="member.uid === adminUid" class="admin-label">(Admin)</span>
+          <button
+            v-if="user.uid === adminUid && member.uid !== adminUid"
+            class="kick-button"
+            @click="handleKickMember(member)"
           >
-            <img :src="member.icon || 'path_to_default_icon_or_gravatar'" class="member-icon" alt="User Icon" />
-            {{ member.name }}
-            <span v-if="member.uid === adminUid" class="admin-label">(Admin)</span>
-            <button
-              v-if="user.uid === adminUid && member.uid !== adminUid"
-              class="kick-button"
-              @click="handleKickMember(member)"
-            >
-              Kick
-            </button>
-          </li>
-        </ul>
-        <button v-if="groupMembers.length !== 0" class="group-button" @click="handleLeaveGroup">Leave Group</button>
-        <hr class="divider" />
-      </div>
+            Kick
+          </button>
+        </li>
+      </ul>
+      <button v-if="groupMembers.length !== 0" class="group-button" @click="handleLeaveGroup">Leave Group</button>
+      <hr class="divider" />
+    </div>
 
-      <!-- Dropdown to select saved group -->
-      <div v-if="savedGroups.length" class="dropdown-container">
-        <label for="saved-group-select">Select a Saved Group:</label>
-        <select id="saved-group-select" @change="handleGroupSelect" class="dropdown">
-          <option value="">-- Select a Group --</option>
-          <option
-            v-for="group in savedGroups"
-            :key="group.key"
-            :value="group.key"
-          >
-            {{ group.name }}
-          </option>
-        </select>
-      </div>
-  
-  
-      <div class="join-input-container">
-        <input
-          type="text"
-          :value="groupMembers.length ? groupKey : writtenGroupKey"
-          placeholder="Join Through Key"
-          class="join-input"
-          id="join-input"
-          @keyup.enter="handleJoinGroup"
-          @input="handleUpdateGroupKey"
-          :disabled="groupMembers.length !==0"
-        />
-      </div>
-  
-      <div v-if="user && groupKey==''" class="create-group-container">
-        <button class="create-group-button" @click="handleRequestCreateGroup">
-          Create Your Own Group
-        </button>
-      </div>
-      <div class="highlighted-places" style="cursor: pointer; user-select: none;" v-if="groupHighlightedPlaces.length > 0">
-    <div 
-      v-for="(place, index) in groupHighlightedPlaces" 
-      :key="index" 
-      class="place-row">
+    <div v-if="savedGroups.length" class="dropdown-container">
+      <label for="saved-group-select">Select a Saved Group:</label>
+      <select id="saved-group-select" @change="handleGroupSelect" class="dropdown">
+        <option value="">-- Select a Saved Group --</option>
+        <option
+          v-for="group in savedGroups"
+          :key="group.key"
+          :value="group.key"
+        >
+          {{ group.name }}
+        </option>
+      </select>
+    </div>
 
-      <img :src="place.icon" alt="Place Icon" class="place-icon" @click="highlightWasClicked(groupHighlightedPlaces[index])"/>
-      
-      <span class="place-name" style="color: white" @click="highlightWasClicked(groupHighlightedPlaces[index])">{{ place.name }}</span>
-
-      <button class="remove-button" @click="removeHighlightForAll(groupHighlightedPlaces[index])">
-        ✖
+    <div class="join-input-container">
+      <input
+        type="text"
+        :value="groupMembers.length ? groupKey : writtenGroupKey"
+        placeholder="Join Through Key"
+        class="join-input"
+        id="join-input"
+        @keyup.enter="handleJoinGroup"
+        @input="handleUpdateGroupKey"
+        :disabled="groupMembers.length !== 0"
+      />
+      <button v-if="groupKey" class="copy-group-key-button" @click="copyGroupKey">
+        Copy Group Key
       </button>
     </div>
-    </div>
-    </div>
-  </template>
 
+    <div v-if="user && groupKey === ''" class="create-group-container">
+      <button class="create-group-button" @click="handleRequestCreateGroup">
+        Create Your Own Group
+      </button>
+    </div>
+
+    <div
+      class="highlighted-places"
+      style="cursor: pointer; user-select: none;"
+      v-if="groupHighlightedPlaces.length > 0"
+    >
+      <div 
+        v-for="(place, index) in groupHighlightedPlaces" 
+        :key="index" 
+        class="place-row"
+      >
+        <img 
+          :src="place.icon" 
+          alt="Place Icon" 
+          class="place-icon" 
+          @click="highlightWasClicked(groupHighlightedPlaces[index])"
+        />
+        <span 
+          class="place-name" 
+          style="color: white" 
+          @click="highlightWasClicked(groupHighlightedPlaces[index])"
+        >
+          {{ place.name }}
+        </span>
+        <button class="remove-button" @click="removeHighlightForAll(groupHighlightedPlaces[index])">
+          ✖
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 import Swal from 'sweetalert2';
 export default {
-  name:'SidebarTest',
-  data(){
+  name: 'SidebarTest',
+  data() {
     return {
       grpNum: 0,
     };
   },
-  mounted() {
-
-    },
-    emits: [
+  mounted() {},
+  emits: [
     'join-group',
     'leave-group',
     'kick-member',
@@ -109,104 +121,113 @@ export default {
     'groupselected',
     'actually-create-group',
     'close-prompt',
-  ], 
-    props: {
-        user: {
-        type: Object,
-        },
-        place: {
-        type: Object,
-        },
-        groupMembers: {
-        type: Array,
-        },
-        savedGroups:{
-        type: Array,
-        },
-        adminUid: {
-        type: String,
-        },
-        prompt: {
-          type: Boolean,
-        },
-        groupKey: {
-        type: String,
-        },
-        writtenGroupKey: {
-        type: String,
-        },
-        kickedMembers: {
-        type: Array,
-        },
-        groupHighlightedPlaces : {
-          type: Array,
-        },
+  ],
+  props: {
+    user: {
+      type: Object,
     },
-    methods: {
-        handleGroupSelect(event) {
-          console.log("Entered dropdown event receiver with value: ", event.target.value);
-          const selectedKey = event.target.value;
-          if (selectedKey) {
-            this.$emit('groupselected', selectedKey); // Emit selected group key
-          }
-        },
-        handleJoinGroup() {
-            this.$emit('join-group');
-        },
-        handleLeaveGroup() {
-            this.$emit('leave-group');
-        },
-        handleKickMember(member) {
-            this.$emit('kick-member', member);
-        },
-        handleRequestCreateGroup() {
-    this.$emit('create-group'); 
-
-          //this uses a custom dialogue component from sweetalert2: https://sweetalert2.github.io/
-    Swal.fire({
-      title: 'Choose a group name',
-      text: 'The invite key is generated separately after this step. Copy and send the key to your friends!',
-      input: 'text', // Enables a prompt input
-      inputPlaceholder: 'Enter group name...',
-      backdrop: true,
-      showCancelButton: true,
-      confirmButtonText: 'OK',
-      color: '#fff',
-      background: '#181A1B',
-      confirmButtonColor: '#9F7AEA',
-      cancelButtonColor: '#FF0000',
-      cancelButtonText: 'Cancel',
-      customClass: {
-        popup: 'custom-dialog-popup', // Custom popup class
-        title: 'custom-dialog-title', // Custom title class
-        input: 'custom-dialog-input', // Custom input field class
-        confirmButton: 'custom-confirm-btn', // Custom confirm button class
-        cancelButton: 'custom-cancel-btn' // Custom cancel button class
-      },
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log('OK clicked:', result.value);
-        // Handle the confirmed value here
-        this.$emit('actually-create-group', result.value || 'Default Group Name');
-      } else if (result.isDismissed) {
-        console.log('User canceled or closed the dialog');
+    place: {
+      type: Object,
+    },
+    groupMembers: {
+      type: Array,
+    },
+    savedGroups: {
+      type: Array,
+    },
+    adminUid: {
+      type: String,
+    },
+    prompt: {
+      type: Boolean,
+    },
+    groupKey: {
+      type: String,
+    },
+    writtenGroupKey: {
+      type: String,
+    },
+    kickedMembers: {
+      type: Array,
+    },
+    groupHighlightedPlaces: {
+      type: Array,
+    },
+  },
+  methods: {
+    handleGroupSelect(event) {
+      console.log("Entered dropdown event receiver with value: ", event.target.value);
+      const selectedKey = event.target.value;
+      if (selectedKey) {
+        this.$emit('groupselected', selectedKey);
       }
-    });
-      },
-        handleMemberWasClicked(member) {
-            this.$emit('clicked-member', member); 
+    },
+    handleJoinGroup() {
+      this.$emit('join-group');
+    },
+    handleLeaveGroup() {
+      this.$emit('leave-group');
+    },
+    handleKickMember(member) {
+      this.$emit('kick-member', member);
+    },
+    handleRequestCreateGroup() {
+      this.$emit('create-group'); 
+
+      Swal.fire({
+        title: 'Choose a group name',
+        text: 'The invite key is generated separately after this step. Copy and send the key to your friends!',
+        input: 'text',
+        inputPlaceholder: 'Enter group name...',
+        backdrop: true,
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        color: '#fff',
+        background: '#181A1B',
+        confirmButtonColor: '#9F7AEA',
+        cancelButtonColor: '#FF0000',
+        cancelButtonText: 'Cancel',
+        customClass: {
+          popup: 'custom-dialog-popup',
+          title: 'custom-dialog-title',
+          input: 'custom-dialog-input',
+          confirmButton: 'custom-confirm-btn',
+          cancelButton: 'custom-cancel-btn'
         },
-        handleUpdateGroupKey(ev){
-            console.log("Input event: ", ev);
-            console.log("Input event target value: ", ev.target.value);
-            this.$emit('keyupdate', ev.target.value);
-        },
-        highlightWasClicked(place){
-          this.$emit('clicked-highlight', place);
-        },
-        removeHighlightForAll(place){
-          this.$emit('remove-highlight', place); 
-        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          console.log('OK clicked:', result.value);
+          this.$emit('actually-create-group', result.value || 'Default Group Name');
+        } else if (result.isDismissed) {
+          console.log('User canceled or closed the dialog');
+        }
+      });
+    },
+    handleMemberWasClicked(member) {
+      this.$emit('clicked-member', member);
+    },
+    handleUpdateGroupKey(ev) {
+      console.log("Input event: ", ev);
+      console.log("Input event target value: ", ev.target.value);
+      this.$emit('keyupdate', ev.target.value);
+    },
+    highlightWasClicked(place) {
+      this.$emit('clicked-highlight', place);
+    },
+    removeHighlightForAll(place) {
+      this.$emit('remove-highlight', place);
+    },
+    copyGroupKey() {
+      if (this.groupKey) {
+        navigator.clipboard.writeText(this.groupKey)
+          .then(() => {
+            console.log('Group key copied to clipboard:', this.groupKey);
+          })
+          .catch((err) => {
+            console.error('Failed to copy group key:', err);
+          });
+      }
+    },
   },
 };
 </script>
